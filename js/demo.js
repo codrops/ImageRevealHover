1d47875c7174624cfba6542bd19d2c2812e880ac
botelho <botelho@users.noreply.github.com>
2018-11-27 13:05:50 +0000

    Added effect11

--- js/demo.js
+++ js/demo.js
@@ -1070,6 +1070,101 @@
             TweenMax.set(this.DOM.reveal, {opacity: 0})
         }
     }
+	
+    // Effect 11
+    class HoverImgFx11 {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.reveal = document.createElement('div');
+            this.DOM.reveal.className = 'hover-reveal';
+            this.DOM.reveal.innerHTML = `<div class="hover-reveal__img" style="background-image:url(${this.DOM.el.dataset.img})"></div>`;
+            this.DOM.el.appendChild(this.DOM.reveal);
+            this.DOM.revealImg = this.DOM.reveal.querySelector('.hover-reveal__img');
+
+            this.initEvents();
+        }
+        initEvents() {
+            this.positionElement = (ev) => {
+                const mousePos = getMousePos(ev);
+                const docScrolls = {
+                    left : document.body.scrollLeft + document.documentElement.scrollLeft, 
+                    top : document.body.scrollTop + document.documentElement.scrollTop
+                };
+                this.DOM.reveal.style.top = `${mousePos.y+20-docScrolls.top}px`;
+                this.DOM.reveal.style.left = `${mousePos.x+20-docScrolls.left}px`;
+            };
+            this.mouseenterFn = (ev) => {
+                this.positionElement(ev);
+                this.showImage();
+            };
+            this.mousemoveFn = ev => requestAnimationFrame(() => {
+                this.positionElement(ev);
+            });
+            this.mouseleaveFn = () => {
+                this.hideImage();
+            };
+            
+            this.DOM.el.addEventListener('mouseenter', this.mouseenterFn);
+            this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
+            this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
+        }
+        showImage() {
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    this.DOM.reveal.style.opacity = 1;
+                    TweenMax.set(this.DOM.el, {zIndex: 1000});
+                }
+            })
+            .add('begin')
+            .set(this.DOM.revealImg, {transformOrigin: '95% 50%', x: '100%'})
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {scaleX: 0.5, scaleY: 1},
+                scaleX: 1.5,
+                scaleY: 0.7,
+                //opacity: 0.8
+            }), 'begin')
+            .add(new TweenMax(this.DOM.revealImg, 0.8, {
+                ease: Expo.easeOut,
+                startAt: {rotation: 10, y: '5%', opacity: 0},
+                rotation: 0,
+                y: '0%',
+                opacity: 1
+            }), 'begin')
+            .set(this.DOM.revealImg, {transformOrigin: '0% 50%'})
+            .add(new TweenMax(this.DOM.revealImg, 0.6, {
+                ease: Expo.easeOut,
+                scaleX: 1,
+                scaleY: 1,
+                opacity: 1
+            }), 'begin+=0.2')
+            .add(new TweenMax(this.DOM.revealImg, 0.6, {
+                ease: Expo.easeOut,
+                x: '0%'
+            }), 'begin+=0.2')
+        }
+        hideImage() {
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: 999});
+                },
+                onComplete: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: ''});
+                    TweenMax.set(this.DOM.reveal, {opacity: 0});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                opacity: 0,
+                x: '-20%'
+            }), 'begin');
+        }
+    }
 
     Array.from(document.querySelectorAll('[data-fx="1"] > a, a[data-fx="1"]')).forEach(link => new HoverImgFx1(link));
     Array.from(document.querySelectorAll('[data-fx="2"] > a, a[data-fx="2"]')).forEach(link => new HoverImgFx2(link));
@@ -1081,6 +1176,7 @@
     Array.from(document.querySelectorAll('[data-fx="8"] > a, a[data-fx="8"]')).forEach(link => new HoverImgFx8(link));
     Array.from(document.querySelectorAll('[data-fx="9"] > a, a[data-fx="9"]')).forEach(link => new HoverImgFx9(link));
     Array.from(document.querySelectorAll('[data-fx="10"] > a, a[data-fx="10"]')).forEach(link => new HoverImgFx10(link));
+    Array.from(document.querySelectorAll('[data-fx="11"] > a, a[data-fx="11"]')).forEach(link => new HoverImgFx11(link));
 
     
     // Demo purspose only: Preload all the images in the page..
